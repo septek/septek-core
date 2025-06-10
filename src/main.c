3b10820872f3fa60f6645b1715f81d06f76c2d00
james <soapy.dev@proton.me>
2025-06-10 10:56:04 -0700

    yeap

--- src/main.c
+++ src/main.c
@@ -1,10 +1,21 @@
 #include "sf/dynamic.h"
+#include "sf/result.h"
+#include <sf/graphics.h>
+#include <stdio.h>
 #include <stdlib.h>
 
 int main() {
-    sf_map map = sf_map_new();
-    sf_map_cinsert(&map, "lol", &sf_lit("fart"));
-    printf("%s\n", sf_map_cget(&map, sf_str, "lol").c_str);
-    sf_map_delete(&map);
-    return EXIT_SUCCESS;
+    mat4 projection;
+    glm_ortho(0, 1280, 0, 720, -10, 100, projection);
+    sf_window *win;
+    sf_camera cam = sf_camera_new(60, projection);
+    sf_result res = sf_window_new(&win, sf_lit("Window"), (sf_vec2){1280, 720}, &cam);
+    if (!res.ok) {
+        perror(res.err.c_str);
+        return EXIT_FAILURE;
+    }
+
+    while (sf_window_loop(win)) {
+        sf_window_swap(win);
+    }
 }

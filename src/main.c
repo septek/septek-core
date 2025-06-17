61f5200dcd557af2d4a673869b9007a652782097
james <jfleischman6@gmail.com>
2025-06-16 22:46:43 -0700

    Reformatted to work better with CLion

--- src/main.c
+++ src/main.c
@@ -1,21 +1,19 @@
-#include "sf/dynamic.h"
 #include "sf/result.h"
-#include <sf/graphics.h>
-#include <stdio.h>
+#include <sf/window.h>
 #include <stdlib.h>
 
 int main() {
     mat4 projection;
     glm_ortho(0, 1280, 0, 720, -10, 100, projection);
     sf_window *win;
-    sf_camera cam = sf_camera_new(60, projection);
-    sf_result res = sf_window_new(&win, sf_lit("Window"), (sf_vec2){1280, 720}, &cam);
+    const sf_result res = sf_window_new(&win, sf_lit("Window"), (sf_vec2){1280, 720});
     if (!res.ok) {
         perror(res.err.c_str);
         return EXIT_FAILURE;
     }
 
+    sf_camera cam = sf_camera_new(60, projection);
     while (sf_window_loop(win)) {
-        sf_window_swap(win);
+        sf_window_draw(win, &cam);
     }
 }
